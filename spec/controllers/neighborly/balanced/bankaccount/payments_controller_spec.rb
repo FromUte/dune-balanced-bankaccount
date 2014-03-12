9c53497e57b84f40b0e3933fb25c83c808ea8824
Josemar Luedke <josemarluedke@gmail.com>
2014-03-12 20:31:07 -0300

    Remove dubplicated specs from payments controller

--- spec/controllers/neighborly/balanced/bankaccount/payments_controller_spec.rb
+++ spec/controllers/neighborly/balanced/bankaccount/payments_controller_spec.rb
@@ -18,78 +18,40 @@ describe Neighborly::Balanced::Bankaccount::PaymentsController do
     controller.stub(:current_user).and_return(current_user)
   end
 
-  describe "GET 'new'" do
-    it 'should fetch balanced customer' do
-      expect_any_instance_of(Neighborly::Balanced::Customer).to receive(:fetch).and_return(customer)
+  describe 'get new' do
+    it 'should use accounts controller new action' do
+      expect_any_instance_of(Neighborly::Balanced::Bankaccount::AccountsController).to receive(:new)
       get :new, contribution_id: 42
-    end
-
-    it 'request should be successfuly' do
-      get :new, contribution_id: 42
-      expect(response.status).to eq 200
-    end
-
-    it 'should assign balanced_marketplace_id to view' do
-      get :new, contribution_id: 42
-      expect(assigns(:balanced_marketplace_id)).to_not be_nil
-    end
-
-    it 'should assign bank_account to view' do
-      get :new, contribution_id: 42
-      expect(assigns(:bank_account)).to_not be_nil
-    end
-
-    it 'should assign customer to view' do
-      get :new, contribution_id: 42
-      expect(assigns(:customer)).to eq customer
     end
   end
 
-  describe "POST 'create'" do
+  describe 'POST create' do
     let(:params) do
       {
         'payment' => {
           'contribution_id' => '42',
-          'use_bank'        => 'BNK_ID',
+          'use_bank'        => 'SOME_BANK',
           'user'            => {}
         },
       }
     end
 
-    describe "insertion of bank account to a customer" do
-      let(:customer) { double('::Balanced::Customer').as_null_object }
-      let(:bank) do
-        double('::Balanced::BankAccount', id: params['payment']['use_bank'])
-      end
-      before do
-        controller.stub(:customer).and_return(customer)
-      end
-
-      context "customer doesn't have the given bank" do
-        before do
-          customer.stub(:bank_accounts).and_return([])
-        end
-
-        it "inserts to customer's bank accounts list" do
-          expect(customer).to receive(:add_bank_account).with(bank.id)
-          post :create, params
-        end
-      end
-
-      context "customer already has the bank" do
-        before do
-          customer.stub(:bank_accounts).and_return([bank])
-        end
-
-        it "skips insertion" do
-          expect(customer).to_not receive(:add_bank_account)
-          post :create, params
-        end
+    context 'successful' do
+      before { post :create, params }
+      it 'redirects to contribute page' do
+        expect(response).to redirect_to('/projects/forty-two/contributions/42')
       end
     end
 
-    describe "update customer" do
-      it "update user attributes and balanced customer" do
+    describe 'insertion of bank account to a customer' do
+      it 'should use accounts controller attach_bank_to_customer method' do
+        expect_any_instance_of(Neighborly::Balanced::Bankaccount::AccountsController).to receive(:attach_bank_to_customer)
+        post :create, params
+      end
+    end
+
+    describe 'update customer' do
+      it 'update user attributes and balanced customer' do
         expect_any_instance_of(Neighborly::Balanced::Customer).to receive(:update!)
         post :create, params
       end
