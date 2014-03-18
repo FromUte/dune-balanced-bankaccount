49a47240ef093ae19d9c1726c5fcc466a2878949
Josemar Luedke <josemarluedke@gmail.com>
2014-03-18 20:33:29 -0300

    Add method to check bank account availability

--- app/controllers/neighborly/balanced/bankaccount/confirmations_controller.rb
+++ app/controllers/neighborly/balanced/bankaccount/confirmations_controller.rb
@@ -1,15 +1,30 @@
 module Neighborly::Balanced::Bankaccount
   class ConfirmationsController < ApplicationController
+    before_action :check_bank_account_availability, only: :new
+
     def new
-      @bank_account = customer.bank_accounts.try(:last)
+      @verification = bank_account.verifications.try(:last)
       render layout: 'application'
     end
 
-    def create
-
-    end
+    def create; end
 
     private
+    def check_bank_account_availability
+      if bank_account
+        if bank_account.verifications.try(:last).try(:state) == 'verified'
+          flash.alert = t('.errors.already_confirmed')
+        end
+      else
+        flash.alert = t('.errors.bank_account_not_found')
+      end
+      redirect_to main_app.payments_user_path(current_user) if flash.alert.present?
+    end
+
+    def bank_account
+      @bank_account ||= customer.bank_accounts.try(:last)
+    end
+
     def customer
       @customer ||= Neighborly::Balanced::Customer.new(current_user, params).fetch
     end
--- spec/controllers/neighborly/balanced/bankaccount/confirmations_controller_spec.rb
+++ spec/controllers/neighborly/balanced/bankaccount/confirmations_controller_spec.rb
@@ -4,9 +4,12 @@ describe Neighborly::Balanced::Bankaccount::ConfirmationsController do
   routes { Neighborly::Balanced::Bankaccount::Engine.routes }
 
   let(:current_user) { double('User').as_null_object }
+  let(:verification) { double('::Balanced::Verification', state: 'unverified') }
   let(:customer) do
     double('::Balanced::Customer',
-           bank_accounts: [double('::Balanced::BankAccount', uid: '/ABANK')],
+           bank_accounts: [double('::Balanced::BankAccount', uid: '/ABANK',
+                                  verifications: [verification]
+                                 )],
            uri:           '/qwertyuiop').as_null_object
   end
 
@@ -18,26 +21,61 @@ describe Neighborly::Balanced::Bankaccount::ConfirmationsController do
   end
 
   describe 'GET new' do
-    it 'should fetch balanced customer' do
-      expect_any_instance_of(
-        Neighborly::Balanced::Customer
-      ).to receive(:fetch).and_return(customer)
-      get :new, contribution_id: 42
+    context 'when user has a bank account' do
+      it 'should fetch balanced customer' do
+        expect_any_instance_of(
+          Neighborly::Balanced::Customer
+        ).to receive(:fetch).and_return(customer)
+        get :new
+      end
+
+      it 'should render application layout' do
+        get :new
+        expect(response).to render_template(layout: 'application')
+      end
+
+      it 'should complete the request successfully' do
+        get :new
+        expect(response.status).to eq 200
+      end
+
+      it 'should assign bank_account to view' do
+        get :new
+        expect(assigns(:bank_account)).to_not be_nil
+      end
+
+      it 'should assign customer to view' do
+        get :new
+        expect(assigns(:customer)).to eq customer
+      end
     end
 
-    it 'should complete the request successfully' do
-      get :new, contribution_id: 42
-      expect(response.status).to eq 200
+    context 'when user do not have a bank account' do
+      before { customer.stub(:bank_accounts).and_return([]) }
+
+      it 'should redirect to user payments page' do
+        get :new
+        expect(response).to redirect_to(/users\/(.+)\/payments/)
+      end
+
+      it 'should set a flash message' do
+        get :new
+        expect(flash.alert).to eq "You don't have any bank account to confirm, please add one."
+      end
     end
 
-    it 'should assign bank_account to view' do
-      get :new, contribution_id: 42
-      expect(assigns(:bank_account)).to_not be_nil
-    end
+    context 'when user has a bank account already confirmed' do
+      let(:verification) { double('::Balanced::Verification', state: 'verified') }
 
-    it 'should assign customer to view' do
-      get :new, contribution_id: 42
-      expect(assigns(:customer)).to eq customer
+      it 'should redirect to user payments page' do
+        get :new
+        expect(response).to redirect_to(/users\/(.+)\/payments/)
+      end
+
+      it 'should set a flash message' do
+        get :new
+        expect(flash.alert).to eq 'Your Bank Account was already confirmed.'
+      end
     end
   end
 end
