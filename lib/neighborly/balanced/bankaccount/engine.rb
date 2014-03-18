6e75745beb41810cd5cb85e6ade3e52abca2f7a0
Josemar Luedke <josemarluedke@gmail.com>
2014-03-18 13:51:16 -0300

    Load helpers from main application
    With this we are able to use helpers that is on the main application.
    It also allow us to render the application layout here in the engine.

--- lib/neighborly/balanced/bankaccount/engine.rb
+++ lib/neighborly/balanced/bankaccount/engine.rb
@@ -3,6 +3,11 @@ module Neighborly
     module Bankaccount
       class Engine < ::Rails::Engine
         isolate_namespace Neighborly::Balanced::Bankaccount
+        initializer 'action_controller' do |app|
+          ActiveSupport.on_load :action_controller do
+            helper Rails.application.helpers
+          end
+        end
       end
     end
   end
