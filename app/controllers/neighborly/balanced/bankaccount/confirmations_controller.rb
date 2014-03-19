b5dbb299f35db6c4af6d699e8cebb3dc177f3d40
Josemar Luedke <josemarluedke@gmail.com>
2014-03-19 00:13:25 -0300

    Create the confirmation of bank account verifications
    It also create new Bank Account verification after 3 failed attempts (close #19)

--- app/controllers/neighborly/balanced/bankaccount/confirmations_controller.rb
+++ app/controllers/neighborly/balanced/bankaccount/confirmations_controller.rb
@@ -4,27 +4,50 @@ module Neighborly::Balanced::Bankaccount
     before_action :check_bank_account_availability, only: :new
 
     def new
-      @verification  = bank_account.verifications.try(:last)
+      @verification  = verification
       @contributions = current_user.contributions.with_state(:payment_authorized)
       render layout: 'application'
     end
 
-    def create; end
+    def create
+      verification.confirm(params[:confirmation][:amount_1],
+                           params[:confirmation][:amount_2])
+
+      flash.notice = t('.messages.success')
+      redirect_to main_app.payments_user_path(current_user)
+    rescue Balanced::BankAccountVerificationFailure
+      flash.alert = t('.messages.unnable_to_verify')
+      check_for_remaining_attempts
+      redirect_to new_confirmation_path
+    end
 
     private
+    def check_for_remaining_attempts
+      if verification.remaining_attempts == 0
+        flash.alert = t('.messages.not_remaining_attempts')
+        bank_account.verify
+      end
+    end
+
     def check_bank_account_availability
       if bank_account
-        if bank_account.verifications.try(:last).try(:state) == 'verified'
+        if verification.try(:state) == 'verified'
           flash.alert = t('.errors.already_confirmed')
+          error = true
         end
       else
         flash.alert = t('.errors.bank_account_not_found')
+        error = true
       end
-      redirect_to main_app.payments_user_path(current_user) if flash.alert.present?
+      redirect_to main_app.payments_user_path(current_user) if error == true
+    end
+
+    def verification
+      @verification ||= bank_account.verifications.try(:first)
     end
 
     def bank_account
-      @bank_account ||= customer.bank_accounts.try(:last)
+      @bank_account ||= customer.bank_accounts.try(:first)
     end
 
     def customer
