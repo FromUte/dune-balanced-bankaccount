ba72333b47687c5270bee467d8d3c2900ff5a481
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-03-20 19:57:40 -0300

    Mount neighborly-balanced gem in dummy app

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount Neighborly::Balanced::Bankaccount::Engine => '/', as: 'neighborly_balanced_bankaccount'
+  mount Neighborly::Balanced::Engine              => '/', as: 'neighborly_balanced'
 
   resources :projects do
     resources :contributions
