e86c178c61bc6ee6d2b3aade57a184428d13c5a2
Josemar Luedke <josemarluedke@gmail.com>
2014-04-21 13:14:47 -0300

    Add meta informations to debit

--- app/models/neighborly/balanced/bankaccount/payment.rb
+++ app/models/neighborly/balanced/bankaccount/payment.rb
@@ -5,7 +5,8 @@ module Neighborly::Balanced::Bankaccount
                                source_uri: debit_resource_uri,
                                appears_on_statement_as: ::Configuration[:balanced_appears_on_statement_as],
                                description: debit_description,
-                               on_behalf_of_uri: project_owner_customer.uri)
+                               on_behalf_of_uri: project_owner_customer.uri,
+                               meta: meta)
       @contribution.confirm!
     rescue Balanced::BadRequest
       @status = :failed
@@ -40,6 +41,34 @@ module Neighborly::Balanced::Bankaccount
     def project_owner_customer
       @project_owner_customer ||= Neighborly::Balanced::Customer.new(
         @contribution.project.user, {}).fetch
+    end
+
+    def meta
+      {
+        payment_service_fee: fee_calculator.fees,
+        payment_service_fee_paid_by_user: @attrs[:pay_fee],
+        project: {
+          id:        @contribution.project.id,
+          name:      @contribution.project.name,
+          permalink: @contribution.project.permalink,
+          user:      @contribution.project.user.id
+        },
+        user: {
+          id:        @contribution.user.id,
+          name:      @contribution.user.display_name,
+          email:     @contribution.user.email,
+          address:   { line1:        @contribution.user.address_street,
+                       city:         @contribution.user.address_city,
+                       state:        @contribution.user.address_state,
+                       postal_code:  @contribution.user.address_zip_code
+          }
+        },
+        reward: {
+          id:          @contribution.reward.try(:id),
+          title:       @contribution.reward.try(:title),
+          description: @contribution.reward.try(:description)
+        }
+      }
     end
   end
 end
