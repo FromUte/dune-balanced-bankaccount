27da7c77468084fecb4bbf963e1932108bda12ea
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-08-21 14:31:23 -0300

    Manually migrate data of uris to hrefs in database

--- db/migrate/20140813180429_use_hrefs_for_balanced_resources.rb
+++ db/migrate/20140813180429_use_hrefs_for_balanced_resources.rb
@@ -1,31 +1,40 @@
 class UseHrefsForBalancedResources < ActiveRecord::Migration
   def up
-    add_column :balanced_contributors, :href, :string
-    add_column :balanced_contributors, :bank_account_href, :string
-
-    fetch_hrefs
-
-    remove_column :balanced_contributors, :uri
-    remove_column :balanced_contributors, :bank_account_uri
+    rename_column :balanced_contributors, :uri, :href
+    rename_column :balanced_contributors, :bank_account_uri, :bank_account_href
+    migrate_uris(:up)
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    rename_column :balanced_contributors, :href, :uri
+    rename_column :balanced_contributors, :bank_account_href, :bank_account_uri
+    migrate_uris(:down)
   end
 
   private
 
-  def fetch_hrefs
-    Neighborly::Balanced::Contributor.transaction do
-      Neighborly::Balanced::Contributor.all.each do |contributor|
-        if contributor.bank_account_uri.present?
-          bank_account = Balanced::BankAccount.find(contributor.bank_account_uri)
-          contributor.bank_account_href = bank_account.href
-        end
-        customer = Balanced::Customer.find(contributor.uri)
-        contributor.href = customer.href
-        status = contributor.save(validate: false)
-        Rails.logger.info "Migrated bank_account_href of Contributor ##{contributor.id}. Successful? #{status}"
+  def migrate_uris(direction)
+    Neighborly::Balanced::Contributor.reset_column_information
+    Neighborly::Balanced::Contributor.all.each do |contributor|
+      migrate_attr(contributor, :bank_account_uri, :bank_account_href, direction)
+      migrate_attr(contributor, :uri, :href, direction)
+      status = contributor.save(validate: false)
+      Rails.logger.info "Migrated Contributor ##{contributor.id}. Successful? #{status}"
+    end
+  end
+
+  def migrate_attr(object, fst_attr, snd_attr, direction)
+    if direction.eql? :up
+      attr = snd_attr
+      old_value = object.send(attr)
+      if old_value.present?
+        object.public_send("#{attr}=", old_value.sub('/v1', ''))
+      end
+    else
+      attr = fst_attr
+      old_value = object.send(attr)
+      if old_value.present?
+        object.public_send("#{attr}=", '/v1' + old_value)
       end
     end
   end
